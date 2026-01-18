ac7ea9c706b39a0f074339854ad921b1577efdbe
Zaeem Athar <zaeemathar94@gmail.com>
2026-01-18 22:48:18 +0100

    Updating func name and docstring.

--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -1,8 +1,14 @@
 from pyspark.sql import SparkSession
 
-def get_spark_session(app_name: str) -> SparkSession:
+def create_spark_session(app_name: str) -> SparkSession:
     """
-    Creates or retrieves a SparkSession with environment-specific configs.
+    Creates a SparkSession.
+
+    Parameters:
+    - app_name: Name of the Spark application
+    
+    Returns:
+    - SparkSession object
     """
     return (
         SparkSession.builder
